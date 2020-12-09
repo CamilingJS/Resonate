f434505bd3f59b46405a39a6541b5647ecbf2eab
Jonathan <jonathancamiling@gmail.com>
2020-12-08 22:48:17 -0800

    initiated js file

---
+++ script.js
@@ -0,0 +1,12 @@
+const main = document.querySelector('main');
+const voicesSelect = document.getElementById('voices');
+const textarea = document.getElementById('text');
+const readBtn = document.getElementById('read');
+const toggleBtn = document.getElementById('toggle');
+const closeBtn = document.getElementById('close');
+
+const data = [
+    {
+        imgae:''
+    }
+];
